6c0832521b63cd18b73514ba5acf71a6dc960651
Luiz Karpinski <lkastabackup@gmail.com>
2022-11-03 21:46:33 -0300

    Update JS

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
 var i = 0
 const type = ["dourado", "explorer", "ignite"]
+const textinho = document.querySelector('.form__field') 
 
-function changeCard(event) {
-    if (i > 2) {
-        i = 0
-    }
-    const card = document.querySelector(".card")
-    card.style.backgroundImage = `url(./assets/bg-${type[i]}.svg)`
-    i++
-}
+function setImageDefault() {
+   img.setAttribute('src', `https://i1.sndcdn.com/artworks-000501300870-udhbt5-t500x500.jpg`);
+ }
+
+// function setImageByNick() {
+//   img.setAttribute('src', `https://github.com/${nick.value}.png`);
+// }
+
+// function changeCard(event) {
+//     if (i > 2) {
+//         i = 0
+//     }
+//     const card = document.querySelector(".card")
+//     card.style.backgroundImage = `url(./assets/bg-${type[i]}.svg)`
+//     i++
+// }
+
 
 var nick = document.querySelector(".form__group > input")
 
 var img = document.querySelector('.card > img')
-var imgErrado = document.querySelector('.card > img')
 
-nick.addEventListener("input", (event) => {
-    if (nick.value.length === 0) {
-        img.setAttribute('src', `https://i1.sndcdn.com/artworks-000501300870-udhbt5-t500x500.jpg`);
-    }
-    else if (imgErrado.onerror) {
-        img.setAttribute('src', `https://i1.sndcdn.com/artworks-000501300870-udhbt5-t500x500.jpg`);
-    }
-    else {
-        img.setAttribute('src', `https://github.com/${nick.value}.png`);
-    }
-})  
+img.onerror = function setImagee(){setImageDefault()}; 
+
+
+//GITHUB API
+
+nick.addEventListener("textinho", (event) => {
+    conectionApi();
+})
+
+function conectionApi() {
+
+    const nameUser = document.querySelector(".info > h1")
+    const profileImage = document.querySelector('.card > img')
+
+    let user = textinho.value
+    const url = `https://api.github.com/users/${user}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            if (data.message !== 'Not Found'){
+                nameUser.textContent = data.name
+                profileImage.src = data.avatar_url
+            } else {
+               console.log("jonas")
+            }
+        })
+}
+
+nick.addEventListener("textinho", (event) => {
+    conectionApi();
+})
